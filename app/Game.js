30fbfe8d1dfeff350f74ab84f29970a11686ef22
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2021-03-17 18:23:25 +0100

    add styles and create class Game

--- app/Game.js
+++ app/Game.js
@@ -1,1 +1,27 @@
-console.log("object");
+class Game {
+    constructor({
+        lettersWrapper,
+        categoryWrapeer,
+        wordWrapper,
+        outputWrapper
+    }) {
+        this.lettersWrapper = lettersWrapper;
+        this.categoryWrapeer = categoryWrapeer;
+        this.wordWrapper = wordWrapper;
+        this.outputWrapper = outputWrapper;
+    }
+
+    guess(label) {
+        console.log(label);
+    }
+
+    start() {
+        for (let i = 0; i < 26; i++) {
+            const label = (i + 10).toString(36);
+            const button = document.createElement('button');
+            button.innerHTML = label;
+            button.addEventListener('click', () => this.guess(label));
+            this.lettersWrapper.appendChild(button);
+        }
+    }
+}
