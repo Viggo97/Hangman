df0bdd18a065b9f285c2c2d9a5519c46ac2a8b02
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2021-03-18 10:08:29 +0100

    add quote.js

--- app/Game.js
+++ app/Game.js
@@ -1,21 +1,45 @@
-class Game {
+import {
+    Quote
+} from './Quote.js';
+
+export class Game {
+
+    quotes = [{
+        text: 'pan tadeusz',
+        category: 'Utwór literacki'
+    }, {
+        text: 'janko muzykant',
+        category: 'Utwór literacki',
+    }, {
+        text: 'żółć',
+        category: 'Trudne'
+    }];
+
     constructor({
         lettersWrapper,
-        categoryWrapeer,
+        categoryWrapper,
         wordWrapper,
         outputWrapper
     }) {
         this.lettersWrapper = lettersWrapper;
-        this.categoryWrapeer = categoryWrapeer;
+        this.categoryWrapper = categoryWrapper;
         this.wordWrapper = wordWrapper;
         this.outputWrapper = outputWrapper;
+
+        const {
+            text,
+            category
+        } = this.quotes[Math.floor(Math.random() * this.quotes.length)];
+        this.categoryWrapper.innerHTML = category;
+        this.quote = new Quote(text);
     }
 
-    guess(label) {
-        console.log(label);
+    guess(letter) {
+        this.quote.guess(letter);
+        this.drawQuote();
     }
 
-    start() {
+    drawLetters() {
         for (let i = 0; i < 26; i++) {
             const label = (i + 10).toString(36);
             const button = document.createElement('button');
@@ -24,4 +48,23 @@ class Game {
             this.lettersWrapper.appendChild(button);
         }
     }
+
+    drawQuote() {
+        const content = this.quote.getContent();
+        this.wordWrapper.innerHTML = content;
+    }
+
+    start() {
+        this.drawLetters();
+        this.drawQuote();
+    }
 }
+
+const game = new Game({
+    lettersWrapper: document.querySelector('.letters'),
+    categoryWrapper: document.querySelector('.category'),
+    wordWrapper: document.querySelector('.word'),
+    outputWrapper: document.querySelector('.output'),
+});
+
+game.start();
