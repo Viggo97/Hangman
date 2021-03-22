29f7f27769fba059e0744d67a8af3409e21f8167
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2021-03-22 08:27:54 +0100

    Some fixes

--- app/Game.js
+++ app/Game.js
@@ -2,7 +2,9 @@ import {
     Quote
 } from './Quote.js';
 
-export class Game {
+class Game {
+    currentStep = 0;
+    lastStep = 7;
 
     quotes = [{
         text: 'pan tadeusz',
@@ -34,9 +36,17 @@ export class Game {
         this.quote = new Quote(text);
     }
 
-    guess(letter) {
-        this.quote.guess(letter);
-        this.drawQuote();
+    guess(letter, event) {
+        event.target.disabled = true;
+        if (this.quote.guess(letter)) {
+            this.drawQuote();
+        } else {
+            this.currentStep++;
+            document.getElementsByClassName('step')[this.currentStep].style.opacity = 1;
+            if (this.currentStep === this.lastStep) {
+                this.loosing();
+            }
+        }
     }
 
     drawLetters() {
@@ -44,7 +54,7 @@ export class Game {
             const label = (i + 10).toString(36);
             const button = document.createElement('button');
             button.innerHTML = label;
-            button.addEventListener('click', () => this.guess(label));
+            button.addEventListener('click', (event) => this.guess(label, event));
             this.lettersWrapper.appendChild(button);
         }
     }
@@ -52,11 +62,25 @@ export class Game {
     drawQuote() {
         const content = this.quote.getContent();
         this.wordWrapper.innerHTML = content;
+        if (!content.includes('_')) {
+            this.winning();
+        }
     }
 
     start() {
+        document.getElementsByClassName('step')[this.currentStep].style.opacity = 1;
         this.drawLetters();
         this.drawQuote();
+    }
+
+    winning() {
+        this.wordWrapper.innerHTML = 'WYGRANA!';
+        this.lettersWrapper.innerHTML = '';
+    }
+
+    loosing() {
+        this.wordWrapper.innerHTML = 'PRZEGRANA!';
+        this.lettersWrapper.innerHTML = '';
     }
 }
 
